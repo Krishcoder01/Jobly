3bf617bf60ae07852ceae30d19be3f7b6102c4d9
Krishcoder01 <jaink4474@gmail.com>
2025-01-09 00:54:31 +0530

    Formatting of file

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 //Jobly Platform
 
 
+// All Imports
 const express = require('express');
 const app = express();
 
+
+
+// All Middlewares
+
+
+// All Routes
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// All Error Handlers
+
+// Server Listening
 app.listen(3000, () => {
   console.log('Server is running on port 3000 🚀🚀');
 });
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Hey , 
+This is Krishna Jain :)
+
+I am creating Jobly so that other doesnot faces the problem i find while
+ looking for internship or a projects .
